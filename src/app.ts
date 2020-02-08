9a189ef735c7a4a2a959f619741c17b674661007
RafaelMDM <rafamdmarques@gmail.com>
2020-02-08 16:27:49 -0300

    Clean up ignored files

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import dotenv from "dotenv-safe";
 import routes from "./routes";
 
+const env = dotenv.config();
+const PORT = (process.env.NODE_ENV === 'production') ? process.env.PORT : 3001;
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.listen(PORT);
 
 export default app;
